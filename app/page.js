adef512e9a04dd34215e01407c3cc3769e9aa923
Naimish <naimishbhesaniya212@gmail.com>
2024-04-15 09:43:20 +0530

    add favicon

--- app/page.js
+++ app/page.js
@@ -264,25 +264,25 @@ export default function Home() {
       </div>
       <div className="flex justify-center items-center my-10">
         <div className="text-center flex flex-col justify-center items-center">
-          <h2 className="text-xl font-semibold">Size & Prices</h2>
+          <h2 className="text-3xl mb-2 font-semibold">Size & Prices</h2>
           <table>
             <thead>
               <tr className="bg-blue-800 text-white font-semibold uppercase ">
-                <th class="px-5 py-3 border-e-2 border-white">Type</th>
-                <th className="px-5  border-e-2 py-3">
-                  Total RERA area (sq.ft & m2)
+                <th class="px-2 sm:px-5 py-3 border-e-2 border-white">Type</th>
+                <th className="px-2 sm:px-5  border-e-2 py-3">
+                  Total RERA area
                 </th>
-                <th className="px-5 py-3" colspan="2">
+                <th className="px-2 sm:px-5 py-3" colspan="2">
                   Price
                 </th>
               </tr>
             </thead>
             <tbody className="font-semibold">
               <tr>
-                <td className={`px-5 py-2 text-sm`}>3 BHK City </td>
-                <td className={`px-5 py-2 text-sm `}>1258.3 Sq.ft. </td>
-                <td className={`px-5 py-2 text-sm `}>₹ 5.70 Cr* +</td>
-                <td className={`px-5 py-2 text-sm `}>
+                <td className={`px-2 sm:px-5 py-2 text-sm`}>3 BHK City </td>
+                <td className={`px-2 sm:px-5 py-2 text-sm `}>1258.3 Sq.ft. </td>
+                <td className={`px-2 sm:px-5 py-2 text-sm `}>₹ 5.70 Cr* +</td>
+                <td className={`px-2 sm:px-5 py-2 text-sm hidden sm:block`}>
                   <button
                     onClick={() => {
                       const token = sessionStorage.getItem("Token");
@@ -300,10 +300,10 @@ export default function Home() {
                 </td>
               </tr>
               <tr>
-                <td className={`px-5 py-2 text-sm`}>3 BHK Sea</td>
-                <td className={`px-5 py-2 text-sm `}>1444.9 Sq.ft. </td>
-                <td className={`px-5 py-2 text-sm `}>₹ 6.75 Cr* + </td>
-                <td className={`px-5 py-2 text-sm `}>
+                <td className={`px-2 sm:px-5 py-2 text-sm`}>3 BHK Sea</td>
+                <td className={`px-2 sm:px-5 py-2 text-sm `}>1444.9 Sq.ft. </td>
+                <td className={`px-2 sm:px-5 py-2 text-sm `}>₹ 6.75 Cr* + </td>
+                <td className={`px-2 sm:px-5 py-2 text-sm  hidden sm:block`}>
                   <button
                     onClick={() => {
                       const token = sessionStorage.getItem("Token");
@@ -321,10 +321,10 @@ export default function Home() {
                 </td>
               </tr>
               <tr>
-                <td className={`px-5 py-2 text-sm`}>4 BHK</td>
-                <td className={`px-5 py-2 text-sm `}>1905.1 Sq.ft.</td>
-                <td className={`px-5 py-2 text-sm `}>₹ 9.07 Cr* + </td>
-                <td className={`px-5 py-2 text-sm `}>
+                <td className={`px-2 sm:px-5 py-2 text-sm`}>4 BHK</td>
+                <td className={`px-2 sm:px-5 py-2 text-sm `}>1905.1 Sq.ft.</td>
+                <td className={`px-2 sm:px-5 py-2 text-sm `}>₹ 9.07 Cr* + </td>
+                <td className={`px-2 sm:px-5 py-2 text-sm hidden sm:block`}>
                   <button
                     onClick={() => {
                       const token = sessionStorage.getItem("Token");
@@ -342,10 +342,10 @@ export default function Home() {
                 </td>
               </tr>
               <tr>
-                <td className={`px-5 py-2 text-sm`}>4.5 BHK</td>
-                <td className={`px-5 py-2 text-sm `}>2251.5 Sq.ft.</td>
-                <td className={`px-5 py-2 text-sm `}>₹ 11 Cr* + </td>
-                <td className={`px-5 py-2 text-sm `}>
+                <td className={`px-2 sm:px-5 py-2 text-sm`}>4.5 BHK</td>
+                <td className={`px-2 sm:px-5 py-2 text-sm `}>2251.5 Sq.ft.</td>
+                <td className={`px-2 sm:px-5 py-2 text-sm `}>₹ 11 Cr* + </td>
+                <td className={`px-2 sm:px-5 py-2 text-sm  hidden sm:block`}>
                   <button
                     onClick={() => {
                       const token = sessionStorage.getItem("Token");
@@ -368,7 +368,7 @@ export default function Home() {
       </div>
       <div className="flex justify-center items-center my-10">
         <div className="text-center flex flex-col justify-center items-center">
-          <h2 className="text-xl font-semibold">Rera Number</h2>
+          <h2 className="text-3xl mb-2 font-semibold">Rera Number</h2>
           <p className="font-medium">The Gateway – P51900054969</p>
           <img
             src="/assets/docs/RERA-P51900054969-The-Gateway.webp"
@@ -396,7 +396,7 @@ export default function Home() {
               ></iframe>
             </div>
 
-            <div className="grid grid-cols-2  gap-6 justify-between">
+            <div className="grid  grid-cols-1 sm:grid-cols-2  gap-6 justify-items-center sm:justify-between">
               <div>
                 <h4 className="text-xl font-bold  flex gap-2 mb-3  items-center">
                   <img
